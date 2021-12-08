ee5f245eedd3cb86b07a96a7c6dcea6cc4c42a24
Jack Davidson <jack.davidson@tag-games.com>
2021-12-08 01:13:33 +0000

    completed day 7 part 2 but eh, im not sure what happened...

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,8 +1,15 @@
 use std::fs;
 
 fn main() {
-    let filename = "input.txt";
-    part1(filename);
+    let filename = "test_input.txt";
+    // part1(filename);
+    println!("Sample Input");
+    part2(filename);
+    println!("Correct Answer 168");
+    println!();
+    println!("Puzzle Input");
+    part2("input.txt");
+    println!("Correct Answer 105461913");
 }
 
 fn part1(filename: &str)
@@ -24,4 +31,45 @@ fn part1(filename: &str)
     }
     
     println!("Answer {}", fuel);
+}
+
+fn part2(filename: &str)
+{
+    let positions: Vec<i32> = fs::read_to_string(filename)
+        .expect("Failed to read file.")
+        .split(",")
+        .map(|char| char.parse().expect(format!("Failed to parse {}",char).as_str())).collect();
+    
+    let mut sum : f32 = 0.0;
+    for &p in &positions
+    {
+        sum += p as f32;
+    }
+    let avg = sum / positions.len() as f32;
+    println!("avg {}", avg);
+    println!("rounded avg {}", avg.round());
+    println!("floored avg {}", avg.floor());
+    
+    let floor_avg = avg.floor() as i32;
+    
+    let mut fuel = 0;
+    for val in &positions
+    {
+        let diff = (floor_avg - val).abs();
+        fuel += (1..diff+1).sum::<i32>();
+    }
+    
+    println!("Answer floored {}", fuel);
+
+
+    let round_avg = avg.round() as i32;
+
+    let mut fuel = 0;
+    for val in positions
+    {
+        let diff = (round_avg - val).abs();
+        fuel += (1..diff+1).sum::<i32>();
+    }
+
+    println!("Answer rounded {}", fuel);
 }
